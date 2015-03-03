9442d091049063031d9f09721886df775993c3c0
t.hardcastle <t.hardcastle@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-03-03 11:26:58 +0000

    1.1.2. bugfix in sliceCounts using more than two lengths
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/riboSeqR@100098 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/sliceCounts.R
+++ R/sliceCounts.R
@@ -3,11 +3,11 @@ function(rC, lengths = 27, frames) {
   if(missing(frames)) frames <- list(0:2)
   if(!is.list(frames)) frames <- list(frames)
   if(length(frames) == 1) frames <- rep(frames, length(lengths))
-  if(length(lengths) == 1) lengths <- rep(length, length(frames))
+  if(length(lengths) == 1) lengths <- rep(lengths, length(frames))
   if(length(frames) != length(lengths)) stop("Length of 'lengths' parameter and 'frames' parameter must be equal (or one) if given")
 
   fH <- do.call("cbind", lapply(1:ncol(rC@hits), function(jj) {
-    (do.call("+", lapply(1:length(lengths), function(ll) {
+    (Reduce("+", lapply(1:length(lengths), function(ll) {
       rC@hits[,jj,as.character(frames[[ll]]), as.character(lengths[ll])]
     })))
   }))
@@ -18,5 +18,5 @@ function(rC, lengths = 27, frames) {
 }
 
 rnaCounts <- function(riboDat, CDS) {
-  z <- do.call("cbind", lapply(riboDat@rnaGR, function(x) table(subjectHits(findOverlaps(x, CDS)))))
+  z <- do.call("cbind", lapply(riboDat@rnaGR, function(x) table(factor(subjectHits(findOverlaps(x,CDS)), levels = 1:length(CDS)))))
 }
